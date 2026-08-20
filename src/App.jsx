b95f602576dae10ab65fc31a574921d8ea4591ed
jhonkar11 <jhonkavasquez@gmail.com>
2026-08-20 03:15:46 -0500

    Corregir ruta de importacion de CitasView en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import BottomNav from "./components/layout/BottomNav";
 import ClientCard from "./components/clientes/ClientCard";
 import MapaView from "./components/clientes/MapaView";
 import FormView from "./components/clientes/FormView";
-import CitasView from "./components/clientes/CitasView";
+import CitasView from "./components/citas/CitasView";
 import { ViewHeader, EmptyState, ConfirmModal, TextInput } from "./components/ui/UIKit";
 import { Stamp, IconBtn } from "./components/ui/UIKit";
 
